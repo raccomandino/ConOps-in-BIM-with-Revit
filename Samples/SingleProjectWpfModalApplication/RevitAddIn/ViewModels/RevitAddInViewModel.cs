3418cbb99ce05291633e78caa0cb637553ff0a3e
Dr Francesco Dergano <101935709+raccomandino@users.noreply.github.com>
2024-08-06 03:33:21 +0530

    Add files via upload

---
+++ Samples/SingleProjectWpfModalApplication/RevitAddIn/ViewModels/RevitAddInViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace RevitAddIn.ViewModels;
+
+public sealed partial class RevitAddInViewModel : ObservableObject
+{
+    [ObservableProperty] private string _projectName;
+
+    public RevitAddInViewModel()
+    {
+        ProjectName = Context.Document.ProjectInformation.Name;
+    }
+
+    [RelayCommand]
+    private void SaveProjectName()
+    {
+        var transaction = new Transaction(Context.Document);
+        transaction.Start("Save project name");
+
+        Context.Document.ProjectInformation.Name = ProjectName;
+        
+        transaction.Commit();
+    }
+}
