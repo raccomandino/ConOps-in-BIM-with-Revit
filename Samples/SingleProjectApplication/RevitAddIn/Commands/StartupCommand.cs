560b7e805146d9697c4f6e7b110b46fe27a10751
Dr Francesco Dergano <101935709+raccomandino@users.noreply.github.com>
2024-08-06 03:31:57 +0530

    Add files via upload

---
+++ Samples/SingleProjectApplication/RevitAddIn/Commands/StartupCommand.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.Attributes;
+using Nice3point.Revit.Toolkit.External;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Nice3point.Revit.Toolkit.Options;
+
+namespace RevitAddIn.Commands;
+
+/// <summary>
+///     External command entry point invoked from the Revit interface
+/// </summary>
+[UsedImplicitly]
+[Transaction(TransactionMode.Manual)]
+public class StartupCommand : ExternalCommand
+{
+    public override void Execute()
+    {
+        var selectionConfiguration = new SelectionConfiguration()
+            .Allow.Element(element => element is Wall);
+
+        var reference = UiDocument.Selection.PickObject(ObjectType.Element, selectionConfiguration.Filter);
+        var element = reference.ElementId.ToElement(Document)!;
+
+        TaskDialog.Show("Selected element",element.Name);
+    }
+}
